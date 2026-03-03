a293b06ebf435cd8bb809b1985a54c8af8453073
Abishek J <jabishekabi11@gmail.com>
2026-03-03 15:13:43 +0530

    fixed render port

--- server.js
+++ server.js
@@ -151,8 +151,10 @@ app.post("/login", async (req, res) => {
 });
 
 // ===============================
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
 // ===============================
 // GET USER DATA
